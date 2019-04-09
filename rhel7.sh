0f70544cf0305420154773cab5956d0e37568200
dvasdekis <dimitri@vasdekis.com.au>
2019-04-09 13:12:32 +1000

    Update rhel7.sh

--- rhel7.sh
+++ rhel7.sh
@@ -15,10 +15,10 @@
 #
 ###################### OPTIONAL - Uncomment above if used ########################
 
-# Update all software that shipped with the OS
+echo "Update all software that shipped with the OS"
 yum -y update
 
-# Set Yum to autoupdate at 2am
+echo "Set Yum to autoupdate at 2am"
 cat <<HEREDOC > /etc/crontab.d/update-yum.crontab
 SHELL=/bin/bash 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin 
@@ -27,24 +27,24 @@ HOME=/
 0 2 * * *  yum -y update
 HEREDOC
 
-# Install dependencies - postgresql-libs is needed by postgrest
+echo "Install dependencies - postgresql-libs is needed by postgrest"
 yum -y install postgresql-libs
 
-# Install postgrest
+echo "Install postgrest"
 cd /tmp
 curl -L --remote-name https://github.com/PostgREST/postgrest/releases/download/v5.2.0/postgrest-v5.2.0-centos7.tar.xz
 tar xfJ postgrest-v5.2.0-centos7.tar.xz
 cp postgrest /usr/local/bin
 
-# Define the postgrest service
+echo "Define the postgrest service"
 cat <<HEREDOC > /usr/local/bin/postgrest_service.sh
 postgrest /usr/local/bin/postgrest.conf
 HEREDOC
 
-# Make the postgrest service executable
+echo "Make the postgrest service executable"
 chmod +x /usr/local/bin/postgrest_service.sh
 
-# Install postgrest as a system service
+echo "Install postgrest as a system service"
 cat <<HEREDOC > /etc/systemd/system/postgrest.service
 [Unit]
 Description = Postgrest as an EC2 system service from https://github.com/dvasdekis/postgrest-ec2/
@@ -58,8 +58,8 @@ User = ec2-user
 WantedBy = multi-user.target
 HEREDOC
 
-# Enable the service on boot
+echo "Enable the service on boot"
 systemctl enable postgrest
 
-# Reboot the machine - installs kernel updates and starts postgrest service
+echo "Reboot the machine - installs kernel updates and starts postgrest service"
 reboot
